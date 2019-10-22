e23a7978fd1bd2f86575a975d41669c907e584b1
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-22 13:32:07 -0400

    Hacky conversion function from PROV to CWL TODO: Confirm input and output structures.

--- demo/testdata/minimal-prov-to-cwl.js
+++ demo/testdata/minimal-prov-to-cwl.js
@@ -1,23 +1,93 @@
-export const STEPS = [
-    {
-        "name":"bedtobeddb",
-        "inputs":[
-            {
-                "meta":{ "global": true },
-                "name":"bedfile",
-                "source":[]
-            }
-        ],
-        "outputs":[
-            {
-                "meta":{ "global":true },
-                "name":"beddbfile",
-                "target":[
-                    {
-                        "name":"beddbfile"
-                    }
-                ]
-            }
-        ]
+const prov = {
+    "prefix": {
+        "ex": "http://example.org",
+        "w3": "http://www.w3.org/"
+    },
+    "entity": {
+        "ex:input": {
+            "rdfs:label": "bedfile"
+        },
+        "ex:output": {
+            "rdfs:label": "beddbfile"
+        }
+    },
+    "activity": {
+        "ex:run": {
+            "rdfs:label": "bedtobeddb"
+        }
+    },
+    "wasGeneratedBy": {
+        "_:1": {
+            "prov:activity": "ex:run",
+            "prov:entity": "ex:output"
+        }
+    },
+    "used": {
+        "_:2": {
+            "prov:activity": "ex:run",
+            "prov:entity": "ex:input"
+        }
     }
-];
+}
+
+function getNamesMap(prov, propName) {
+  return Object.fromEntries(
+    Object.entries(prov[propName])
+      .map(
+        ([id, props]) => [id, props['rdfs:label']]
+      )
+  );
+}
+
+function getActivityEntityMap(prov, propName) {
+  return Object.fromEntries(
+    Object.values(prov[propName])
+      .map(
+        (props) => [props['prov:activity'], props['prov:entity']]
+      )
+  );
+}
+
+function getActivityInOut(prov, activityId) {
+  const generatedByMap = getActivityEntityMap(prov, 'wasGeneratedBy');
+  const usedMap = getActivityEntityMap(prov, 'used');
+  return [usedMap[activityId], generatedByMap[activityId]]
+}
+
+function provToCwl(prov) {
+  // TODO: This just handles one example.
+  // Get confirmation that this will be the input structure before investing more time.
+
+  const activityId = 'ex:run'
+  const [entityInputId, entityOutputId] = getActivityInOut(prov, activityId);
+
+  const activityName = prov.activity[activityId]['rdfs:label'];
+  const inputName = prov.entity[entityInputId]['rdfs:label'];
+  const outputName = prov.entity[entityOutputId]['rdfs:label'];
+
+  return [
+      {
+          "name": activityName,
+          "inputs":[
+              {
+                  "meta":{ "global": true },
+                  "name": inputName,
+                  "source":[]
+              }
+          ],
+          "outputs":[
+              {
+                  "meta":{ "global":true },
+                  "name": outputName,
+                  "target":[
+                      {
+                          "name": outputName
+                      }
+                  ]
+              }
+          ]
+      }
+  ];
+}
+
+export const STEPS = provToCwl(prov);
